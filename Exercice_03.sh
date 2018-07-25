c8b5a9b43a68739198c607ebb70df9f97521b5de
EMSC-CSEM <32097208+EMSC-CSEM@users.noreply.github.com>
2018-07-25 14:17:36 +0200

    Ex3 - script to call FDSN ws

---
+++ Exercice_03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#================================
+#Copyright (c) 2018, EPOS Project
+# ---
+# Hands-On Session | Exercice #3
+#================================
+
+url="http://www.seismicportal.eu/fdsnws/event/1/query?start=2017-09-01&end=2017-11-01&format=text&minmag=6.5"
+wget -O result_Exercice_03.txt ${url}
+o=`cat result_Exercice_03.txt`
+echo "$o"
