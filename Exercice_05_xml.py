8285d40c3e962c500836bc3353be12cfb72dc5b7
EMSC-CSEM <32097208+EMSC-CSEM@users.noreply.github.com>
2018-07-25 16:34:53 +0200

    add python code for ex5 includeallorigins

---
+++ Exercice_05_xml.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+================================
+Copyright (c) 2018, EPOS Project
+================================
+---
+includeallorigins case
+unid = '20170919_0000091'
+"""
+
+# -------------------------------------
+# Main functions used in the EUrythmics
+# -------------------------------------
+# Func-Download
+import requests
+def geturl(url):
+    res = requests.get(url, timeout=15)
+    return {'status': res.status_code,
+            'content': res.text}
+
+# Func-Parse (txt)
+import csv
+from io import StringIO
+def parsecsv(txt, usedict=False):
+    if usedict:
+        parser = csv.DictReader(StringIO(txt), delimiter='|')
+    else:
+        parser = csv.reader(StringIO(txt), delimiter='|')        
+    return [line for line in parser]
+
+# Func-Parse (json)
+import json
+def parsejson(txt):
+    return json.loads(txt)
+
+# Func-Parse (xml)
+from obspy import read_events
+def parsexml(url):
+   return read_events(url)
+# ===================================
+
+main_event = {'unid':'20170919_0000091'}
+
+# Download and parse (xml) <= [includeallorigins]
+
+print "\n*** With origins"
+
+url = "http://www.seismicportal.eu/fdsnws/event/1/query\
+?eventid={unid}&format=xml&includeallorigins=true".format(unid=main_event['unid'])
+dataev = read_events(url)
+
+print dataev
+
+for ev in dataev:
+    print ev
+
+
+
+
+
