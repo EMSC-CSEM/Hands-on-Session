4c978538ec0d662c41d2e49674055177b0180a38
EMSC-CSEM <32097208+EMSC-CSEM@users.noreply.github.com>
2018-07-25 15:59:06 +0200

    add python code for ex4

---
+++ Exercice_04_txt.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+================================
+Copyright (c) 2018, EPOS Project
+================================
+---
+Three ways to call the FDSN-event's web service.
+unid = '20170919_0000091'
+"""
+
+# -------------------------------------
+# Main functions used in the EUrythmics
+# -------------------------------------
+# Func-Download
+import requests
+def geturl(url):
+    res = requests.get(url, timeout=15)
+    return {'status': res.status_code,
+            'content': res.text}
+
+# Func-Parse (txt)
+import csv
+from io import StringIO
+def parsecsv(txt, usedict=False):
+    if usedict:
+        parser = csv.DictReader(StringIO(txt), delimiter='|')
+    else:
+        parser = csv.reader(StringIO(txt), delimiter='|')        
+    return [line for line in parser]
+
+# Func-Parse (json)
+import json
+def parsejson(txt):
+    return json.loads(txt)
+
+# Func-Parse (xml)
+from obspy import read_events
+def parsexml(url):
+   return read_events(url)
+# ===================================
+
+
+main_event = {'unid':'20170919_0000091'}
+
+# Download and parse (txt)
+url = "http://www.seismicportal.eu/fdsnws/event/1/query?eventid={unid}&format=text".format(unid=main_event['unid'])
+res = geturl(url)
+dataev = parsecsv(res['content'])
+print("---parse-txt---\n{0}\n".format(dataev))
+
